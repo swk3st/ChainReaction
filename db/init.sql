60b46a4e90639a14fba74e0943dea409b0a1d989
msa8wsy <msa8wsy@virginia.edu>
2021-04-01 00:53:16 -0400

    Changed the way game_ids and chain_ids are stored

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
 USE chain_reaction;
 
 CREATE TABLE IF NOT EXISTS chain (
-    chain_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    chain_id VARCHAR(10) PRIMARY KEY NOT NULL,
     word1 VARCHAR(255) NOT NULL,
     word2 VARCHAR(255) NOT NULL,
     word3 VARCHAR(255) NOT NULL,
@@ -21,18 +21,18 @@ CREATE TABLE IF NOT EXISTS player (
 );
 
 CREATE TABLE IF NOT EXISTS owns (
-    chain_id INT NOT NULL,
+    chain_id VARCHAR(10) NOT NULL,
     player_id INT NOT NULL,
     FOREIGN KEY (chain_id) REFERENCES chain(chain_id),
     FOREIGN KEY (player_id) REFERENCES player(player_id)
 );
 
 CREATE TABLE IF NOT EXISTS game (
-    game_id VARCHAR(255) NOT NULL PRIMARY KEY
+    game_id VARCHAR(10) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS playing (
     player_id INT NOT NULL,
-    game_id INT NOT NULL,
+    game_id VARCHAR(10) NOT NULL,
     team INT NOT NULL
 );
