0f3ab3bae9b050ce3acf0520e3fc636976609919
msa8wsy <msa8wsy@virginia.edu>
2021-03-31 22:45:35 -0400

    Created connect function

--- db/database.php
+++ db/database.php
@@ -32,24 +32,33 @@
 
 $dsn = "mysql:host=$host;dbname=$dbname";
 
+$db = NULL;
+
 /** connect to the database **/
-try 
-{
-   $db = new PDO($dsn, $username, $password);   
-   echo "<p>You are connected to the database</p>";
+function connect () {
+    if(!isset($db)) {
+        try 
+        {
+            $db = new PDO($dsn, $username, $password);   
+            echo "<p>You are connected to the database</p>";
+            return true;
+        }
+        catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+        {
+            // Call a method from any object, 
+            // use the object's name followed by -> and then method's name
+            // All exception objects provide a getMessage() method that returns the error message 
+            $error_message = $e->getMessage();        
+            echo "<p>An error occurred while connecting to the database: $error_message </p>";
+        }
+        catch (Exception $e)       // handle any type of exception
+        {
+            $error_message = $e->getMessage();
+            echo "<p>Error message: $error_message </p>";
+        }
+        return false;
+    }
 }
-catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
-{
-   // Call a method from any object, 
-   // use the object's name followed by -> and then method's name
-   // All exception objects provide a getMessage() method that returns the error message 
-   $error_message = $e->getMessage();        
-   echo "<p>An error occurred while connecting to the database: $error_message </p>";
-}
-catch (Exception $e)       // handle any type of exception
-{
-   $error_message = $e->getMessage();
-   echo "<p>Error message: $error_message </p>";
-}
+
 
 ?>
