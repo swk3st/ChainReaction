5dd9bc3acd469ce9b6944e1b54ac2d4ee6e83279
msa8wsy <msa8wsy@virginia.edu>
2021-04-09 04:17:43 -0400

    Created basic add chain form

--- php/chaininventory.php
+++ php/chaininventory.php
@@ -1,6 +1,12 @@
 <?php
     include "../db/database.php";
+    
+    $status = false;
+    
     function add() {
+        global $status;
+        $status = true;
+        return;
         $playerID = $_POST['playerID'];
         $words = [];
         for ($i = 1; $i <= 7; $i++) {
@@ -9,8 +15,10 @@
         }
         insertChain($playerID, $words);
     }
-
+    
     function update() {
+        global $status;
+        $status = true;
         $chainID = $_POST['chainID'];
         $words = [];
         for ($i = 1; $i <= 7; $i++) {
@@ -31,8 +39,10 @@
     }
 
     function remove() {
+        global $status;
+        $status = true;
         $chainID = $_POST['chainID'];
-        removeChain
+        // removeChain
     }
 
     function doAction($action) {
@@ -48,3 +58,65 @@
     $action = $_POST['action'];
     doAction($action);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel="icon" href="../resources/chainreactionlogo.png">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Solimar Kwa and Michael Asare">
+    <meta name="description" content="Account for Logged In User.">
+    <meta name="keywords" content="Game, Online Game, Chain Reaction">
+
+    <title> Submission </title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="..\css\login.css">
+    <link rel="stylesheet" href="..\css\gameplay.css">
+
+</head>
+
+<header>
+
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+      <a class="navbar-brand" href="home.html">Chain Reaction</a>
+
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="roomcodeplay.html">Play</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="normalcreate.html"> Create </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="account.html"> Account</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  </header>
+
+<body>
+<h1> <?php
+    global $status;
+    if ($status) {
+        echo "Success!";
+    } else {
+        echo "Oops.. Something failed in the process";
+    }
+    ?> </h1>
+<a href="../html/chain-inventory/inventory.php"><button>Back</button></a>
+</body>
+</html>
