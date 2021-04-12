bf597e9a65231aaa74301804bd31344ac72b942e
msa8wsy <msa8wsy@virginia.edu>
2021-04-12 04:10:05 -0400

    Created session.php

---
+++ php/session.php
@@ -0,0 +1,26 @@
+<?php
+
+    // Call this function when the user is beginning their session
+    function login($email, $pwd) {
+        include "../db/database.php";
+        session_start();
+        $playerInfo = getPlayerID($email);
+        if (count($playerInfo) == 0) { 
+            return false;
+        }
+        $player_id = $playerInfo[0]["player_id"];
+        if (!checkPwd($player_id, $pwd)) {
+           return false; 
+        }
+        $_SESSION["playerID"] = $player_id;
+        return true;
+    }
+
+    // Call this function when the user is ending their session
+    function logout() {
+        session_start();
+        $_SESSION = array();
+        session_destroy();
+    }
+
+?>
