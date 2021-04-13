dd2e95d24e64659ff66869d792569b6b4a995e29
msa8wsy <msa8wsy@virginia.edu>
2021-04-12 21:22:52 -0400

    Fixed database mishaps

--- db/database.php
+++ db/database.php
@@ -1,45 +1,44 @@
 <?php
 
-/******************************/
-// connecting to GCP cloud SQL instance
-
-    $username = 'root';
-// $password = 'your-root-password';
-    $password = 'chainreaction';
-
-// $dbname = 'your-database-name';
-    $dbname = 'chain_reaction';
-
-// if PHP is on GCP standard App Engine, use instance name to connect
-// $host = 'instance-connection-name';
-// $host = 'db';
-
-// if PHP is hosted somewhere else (non-GCP), use public IP address to connect
-// $host = "public-IP-address-to-cloud-instance";
-    $host = "34.86.237.81";
-
-
-/******************************/
-// connecting to DB on XAMPP (local)
-
-// $username = 'your-username';
-// $password = 'your-password';
-// $host = 'localhost:3306';
-// $dbname = 'your-database-name';
-
-
-/******************************/
-
-    $dsn = "mysql:host=$host;dbname=$dbname";
-
     $db = NULL;
 
 /** connect to the database **/
 function connect () {
-    global $username, $password, $dbname, $host, $dsn, $db;
+    global $db;
     if(!isset($db)) {
         try 
         {
+        /******************************/
+        // connecting to GCP cloud SQL instance
+
+            $username = 'root';
+        // $password = 'your-root-password';
+            $password = 'chainreaction';
+
+        // $dbname = 'your-database-name';
+            $dbname = 'chain_reaction';
+
+        // if PHP is on GCP standard App Engine, use instance name to connect
+        // $host = 'instance-connection-name';
+        // $host = 'db';
+
+        // if PHP is hosted somewhere else (non-GCP), use public IP address to connect
+        // $host = "public-IP-address-to-cloud-instance";
+            $host = "34.86.237.81";
+
+
+        /******************************/
+        // connecting to DB on XAMPP (local)
+
+        // $username = 'your-username';
+        // $password = 'your-password';
+        // $host = 'localhost:3306';
+        // $dbname = 'your-database-name';
+
+
+        /******************************/
+
+            $dsn = "mysql:host=$host;dbname=$dbname";
             $db = new PDO($dsn, $username, $password);   
             // echo "<p>You are connected to the database</p>";
             return true;
@@ -50,7 +49,7 @@ function connect () {
             // use the object's name followed by -> and then method's name
             // All exception objects provide a getMessage() method that returns the error message 
             $error_message = $e->getMessage();        
-            // echo "<p>An error occurred while connecting to the database: $error_message </p>";
+            echo "<p>An error occurred while connecting to the database: $error_message </p>";
         }
         catch (Exception $e)       // handle any type of exception
         {
@@ -146,8 +145,7 @@ function checkPwd($player_id, $pwd) {
 function getPlayerID($email) {
     global $db;
     connect();
-
-    $sql = "SELECT player_id FROM player WHERE $email = :email";
+    $sql = "SELECT player_id FROM player WHERE email = :email";
     $statement = $db->prepare($sql);
     $statement->bindParam(":email", $email);
     $statement->execute();
