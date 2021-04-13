47f4f028c6ce9576b3a3df6d4d2742ac6676fa95
msa8wsy <msa8wsy@virginia.edu>
2021-04-12 22:59:16 -0400

    Updated php files with navbars

--- html/home.php
+++ html/home.php
@@ -29,30 +29,6 @@
   <header>
     <?php include('../php/navbar.php'); ?>
 
-
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-      <a class="navbar-brand" href="home.html">Chain Reaction</a>
-
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="roomcodeplay.html">Play</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="signup.html">Sign Up</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="login.html">Log In</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
   </header>
 
 
