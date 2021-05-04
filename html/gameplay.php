33283744121698156927ae6fe2016437145bed0f
msa8wsy <msa8wsy@virginia.edu>
2021-05-04 16:34:49 -0400

    Updated styling

--- html/gameplay.php
+++ html/gameplay.php
@@ -6,7 +6,7 @@
     <meta name="author" content="Solimar Kwa and Michael Asare">
     <title> Game Play </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="..\css\button.css">
+    <!-- <link rel="stylesheet" href="..\css\button.css"> -->
     <link rel="stylesheet" href="..\css\gameplay.css">
     <link rel="stylesheet" href="..\css\gamescreen.css">
     <link rel="stylesheet" href="..\css\chain.css">
@@ -20,28 +20,33 @@
   </header>
 
 <body>
-  <h2 id='clock'></h2>
+  <div class='info'>
+    <h2><?php echo $_SESSION['displayName']?><h2>
+    <h2 id='score'></h2>
+    <h2 id='clock'></h2>
+  </div>
+  <br/>
+  <br/>
     <div class='gameplay-container'>
       <div class='left-half'>
-        <h2 id='score'></h2>
-        <button id='above-letter'>Grab an Above Letter</button>
-        <button id='below-letter'>Grab a Below Letter</button>
-        <label for="above-field"><button id='above-guess'>Guess Above</button></label>
+        <button id='above-letter' class='game-button'>Grab an Above Letter</button>
+        <label for="above-field"><button id='above-guess' class='game-button'>Guess Above</button></label>
         <input type="text" id="above-field" name="above-field">
-        <label for="below-field"><button id='below-guess'>Guess Below</button></label>
+        <button id='below-letter' class='game-button'>Grab a Below Letter</button>
+        <label for="below-field"><button id='below-guess' class='game-button'>Guess Below</button></label>
         <input type="text" id="below-field" name="below-field">
         <script type='module' src='../js/gameplay.js'></script>
       </div>
       <div class='right-half'>
       <div class="chain-table">
       <table>
-        <tr><td id="mid1">Word 1</td></tr>
-        <tr><td id="mid2">.</td></tr>
-        <tr><td id="mid3">.</td></tr>
-        <tr><td id="mid4">.</td></tr>
-        <tr><td id="mid5">.</td></tr>
-        <tr><td id="mid6">.</td></tr>
-        <tr><td id="mid7">Word 7</td></tr>
+        <tr id='r1'><td id="word1">Word 1</td></tr>
+        <tr id='r2'><td id="word2">.</td></tr>
+        <tr id='r3'><td id="word3">.</td></tr>
+        <tr id='r4'><td id="word4">.</td></tr>
+        <tr id='r5'><td id="word5">.</td></tr>
+        <tr id='r6'><td id="word6">.</td></tr>
+        <tr id='r7'><td id="word7">Word 7</td></tr>
       </table>
   </div>
       </div>
