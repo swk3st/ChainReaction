70c4c7efe5179350c4afbdc1961abe364edcb453
msa8wsy <msa8wsy@virginia.edu>
2021-05-05 12:34:09 -0400

    Put AJAX in for completed games

--- js/request.js
+++ js/request.js
@@ -340,7 +340,31 @@ function requestPlayerID() {
     });
   }
 
+  function requestAllCompletedGames(playerID) {
+    var xmlhttp = new XMLHttpRequest();
+    return new Promise ((resolve, reject) => {
+  
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+          var data = JSON.parse(xmlhttp.responseText);
+          resolve(data);
+        }
+      }
+      let url = "../php/allcompletedgames.php?playerID=";
+
+      var loc = window.location.pathname;
+      var dir = loc.substring(loc.lastIndexOf('/'));
+      if (dir == "/inventory.php") {
+        url = "../../php/allcompletedgames.php?chainID=";
+      }
+
+      let request = playerID;
+      xmlhttp.open("GET", url+request, true);
+      xmlhttp.send();
+    });
+  }
+
   export { requestPlayerID, requestDisplayName, requestChains, 
     requestGame, playerJoin, startGame, requestStatus, 
     requestPlayers, leaveGame, requestChain, saveHistory, 
-    realTimeUpdate, finishGame, updateCareer};
+    realTimeUpdate, finishGame, updateCareer, requestAllCompletedGames};
