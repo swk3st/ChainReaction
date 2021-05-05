ec799c4182bcd588321521453f789774c817a25a
msa8wsy <msa8wsy@virginia.edu>
2021-05-05 04:09:36 -0400

    Styled match history

--- html/matchhistory.php
+++ html/matchhistory.php
@@ -9,6 +9,7 @@
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/button.css">
     <link rel="stylesheet" href="../css/gameplay.css">
+    <link rel='stylesheet' href='../css/matchhistory.css'>
 
 </head>
 
@@ -35,9 +36,9 @@
 
 <body>
         <h1>Match History: <?php if (isset($gameID)) echo $gameID; ?></h1>
-        <div id='match-history-container'>
-            <div id='left-half'>
-                <table class='nice-table'>
+        <div class='match-history-container'>
+            <div class='left-half'>
+                <table class='nice-table match-history'>
                     <tr>
                         <th>DISPLAY NAME</th>
                         <th>PAYOUT</th>
@@ -53,10 +54,19 @@
                 </table>
             </div>
 
-            <div id='right-half'>
-
+            <div class='right-half'>
+                <div class="chain-table">
+                    <table>
+                        <tr id='row1'><td id="word1">~</td></tr>
+                        <tr id='row2'><td id="word2">~</td></tr>
+                        <tr id='row3'><td id="word3">~</td></tr>
+                        <tr id='row4'><td id="word4">~</td></tr>
+                        <tr id='row5'><td id="word5">~</td></tr>
+                        <tr id='row6'><td id="word6">~</td></tr>
+                        <tr id='row7'><td id="word7">~</td></tr>
+                    </table>
+                </div>
             </div>
         </div>
-        <div></div>
 </body>
 </html>
