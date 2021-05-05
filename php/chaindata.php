667b71d76734b7f444490d5a68417a671c0b1e8a
msa8wsy <msa8wsy@virginia.edu>
2021-05-05 02:33:12 -0400

    Another commit

--- php/chaindata.php
+++ php/chaindata.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
+header('Access-Control-Max-Age: 1000');  
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+
     include "..\db\database.php";
     if (isset($_GET['chainID'])) {
         $data = getChain($_GET['chainID']);
--- php/sessiondata.php
+++ php/sessiondata.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
+header('Access-Control-Max-Age: 1000');  
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+
     session_start();
     if (isset($_GET['var'])) {
         echo json_encode($_SESSION[$_GET['var']]);
