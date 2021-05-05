a8546b8d6f326c007eab91dbe4c7b81076cb2520
msa8wsy <msa8wsy@virginia.edu>
2021-05-04 23:37:47 -0400

    Added back buttons

--- html/chain-inventory/updatechain.php
+++ html/chain-inventory/updatechain.php
@@ -47,7 +47,15 @@
       <input type="hidden" id="chainID" name="chainID" value="<?php if (isset($_GET["chainID"])) echo $_GET["chainID"]?>"/>
       <br/>
       <input type="submit" value="Submit"/>
+      <button id='back'>Back</button>
   </form>
 </div>
+<script>
+  let button = document.getElementById('back');
+  button.addEventListener('click', () => {
+    location.href = './inventory.php';
+  });
+
+</script>
 </body>
 </html>
